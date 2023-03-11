72c74e1ec35b4ba121c66b63d516881a80a6bc89
shiqingshan <1147863825@qq.com>
2023-03-11 23:38:13 +0800

    coolzhul
    init

---
+++ coolzhul-platform/coolzhul-admin/src/main/java/com/coolzhul/admin/ApplicationAdminServer.java
@@ -0,0 +1,15 @@
+package com.coolzhul.admin;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hezw
+ */
+@SpringBootApplication
+public class ApplicationAdminServer {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationAdminServer.class,args);
+    }
+}

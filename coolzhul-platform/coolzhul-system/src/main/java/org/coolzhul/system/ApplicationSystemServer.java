4ff94c65a867e2083c9c8c2e1956083cf1b2e667
shiqingshan <1147863825@qq.com>
2023-11-23 23:31:02 +0800

    1

---
+++ coolzhul-platform/coolzhul-system/src/main/java/org/coolzhul/system/ApplicationSystemServer.java
@@ -0,0 +1,11 @@
+package org.coolzhul.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationSystemServer {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationSystemServer.class,args);
+    }
+}
